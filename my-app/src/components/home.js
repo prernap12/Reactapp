2459f6a4d99beed2c56478b0b0e0131630a19ef4
prernap12 <prernaphunyal@gmail.com>
2025-05-20 21:54:59 +0545

    home page with sliders

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,10 +1,65 @@
 import React from "react";
 import Navbar from './navbar';
+
+// Import Swiper styles
+import { Swiper,SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
+
+import { Autoplay } from "swiper/modules";
+
 const Home = () => {
+    const swiperOptionOne ={
+        breakpoints:{
+            0:{
+                slidesPerView:1,
+
+            },
+            1024:{
+                slidesPerView:3,
+            }
+        },
+        loop:true,
+    };
     return (
-        <div>
+        <div className="home-container">
            <Navbar />
+           <section className="home" id="home">
+            <div className="row">
+                <div className="content">
+                    <p> jkfajkjfkljaflkjakjfklajfklajfkjasfkljklsfjlksjflnsvlknsdljfndkljfnkldsnflndfklnfdlsnlfdn</p>
+                    <a href="\#" className="btn">Shop Now</a>
+                </div>
+                <div className="swiper">
+                     <Swiper
+                        watchSlidesProgress={true}
+                        slidesPerView={3}
+                        autoplay={{
+                            delay:9500,
+                            disableOnInteraction:false,
+                        }}
+                        pagination={{
+                        clickable: true,
+                        }}
+                        modules={[Autoplay]}
+                        className="book-list"
+                        { ...swiperOptionOne}
+                    >
+                        <a href="\#" className="swiper-slide">
+                        <SwiperSlide><img src="image\book1.jpg" alt=""/></SwiperSlide> </a>
+                        <a href="\#" className="swiper-slide">
+                        <SwiperSlide><img src="image\book2.jpg" alt=""/></SwiperSlide> </a>
+                        <a href="\#" className="swiper-slide">
+                        <SwiperSlide><img src="image/book3.jpg" alt=""/></SwiperSlide> </a>
+                        <a href="\#" className="swiper-slide">
+                        <SwiperSlide><img src="image/book4.jpg" alt=""/></SwiperSlide> </a>
+                        <a href="\#" className="swiper-slide">
+                        <SwiperSlide><img src="image/book5.jpg" alt=""/></SwiperSlide> </a>
+                        
+                    </Swiper>
+              </div>
+            </div>
+           </section>
          </div>
     )
 }
-export default Home
+export default Home;
--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,8 +1,18 @@
-import React from "react";
-import { FaSearch, FaHeart, FaUser,FaBook,FaHome,FaList,FaTags, FaStore} from "react-icons/fa";
+import React, {useRef} from "react"
+import { FaSearch, FaHeart, FaUser,FaBook,FaHome,FaList,FaTags, FaStore,FaTimes} from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa";
 
 const Navbar = () => {
+
+    const navRef =useRef(null);
+
+    const showNavbar = () => {
+      if (navRef.current) {
+  /*  menuRef.current.classList.toggle("login-form");*/
+        navRef.current.classList.toggle("login-form")
+          document.body.classList.toggle("login-active"); 
+    }
+}
     return (
         <div>
             <header className="header">
@@ -16,7 +26,7 @@ const Navbar = () => {
                         <div id="search-btn"><FaSearch/></div>
                         <a href="/#"><FaHeart /></a>
                         <a href="/#"><FaCartArrowDown/></a>
-                        <div id="login-btn" className="user-btn"><FaUser /></div>
+                        <div id="login-btn" className="user-btn" onClick={showNavbar}><FaUser /></div>
                     </div>
                 </div>
                 <div className="header_two"> 
@@ -30,14 +40,39 @@ const Navbar = () => {
                     </div>
                 </div>
             </header>
+             
             <div className="bottom-navbar">
+                
                 <a href="/#"><FaHome /></a>
                 <a href="/#"><FaList /></a>
                 <a href="/#"><FaTags /></a>
                 <a href="/#"><FaStore /></a>
 
             </div>
+
+           < div className="login-form-container" ref={navRef}>
+           <div id="close-login-btn" onClick={showNavbar}><FaTimes/></div>
+           <form action="#">
+            <h3>sign in</h3>
+            <span>email</span>
+            <input type="email" className="box" placeholder="enter your email"/>
+            <span>password</span>
+            <input type="password" className="box" placeholder="enter your password"/>
+
+            <div className="check-box">
+                <input type="checkbox" id="remember-me"/>
+                <label htmlFor="">remember me</label>
+            </div>
+
+            <input type="submit " value="sign in" className="btn"/>
+            <p> forget password <a href="\#">click here</a></p>
+            <p>don't have an account? <a href="\#"> create one </a></p>
+            <p></p>
+           </form>
+           </div>
+
         </div>
     )
 }
-export default Navbar
+
+export default Navbar;
